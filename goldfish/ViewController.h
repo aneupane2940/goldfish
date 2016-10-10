f62cc1a4c7befe50e266b126404f06e38f0ff5b4
achyut neupane <neupaneachyut@yahoo.com>
2016-10-10 02:39:05 -0700

    Initial Commit

---
+++ goldfish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  goldfish
+//
+//  Created by achyut neupane on 10/10/16.
+//  Copyright © 2016 achyut neupane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ goldfish/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  goldfish
+//
+//  Created by achyut neupane on 10/10/16.
+//  Copyright © 2016 achyut neupane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
